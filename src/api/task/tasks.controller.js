9b22f307f917db2025b797b55c87675f97fc9cee
Alberto Polo <bertopolo91@gmail.com>
2024-02-02 20:10:30 +0100

    controllers

---
+++ src/api/task/tasks.controller.js
@@ -0,0 +1,55 @@
+import Task from "./task.routes.js"
+
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find()
+    res.json(tasks)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id)
+    if (task) {
+      res.json(task)
+    } else {
+      res.status(404).json({ message: "Task not found" })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const createTask = async (req, res) => {
+  const task = new Task({
+    title: req.body.title,
+    task: req.body.task,
+  })
+
+  try {
+    const newTask = await task.save()
+    res.status(201).json(newTask)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+export const updateTask = async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.json(updatedTask)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+export const deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id)
+    res.json({ message: "Task deleted" })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
---
+++ src/api/user/users.controller.js
@@ -0,0 +1,55 @@
+import User from "./task.routes.js"
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (user) {
+      res.json(user)
+    } else {
+      res.status(404).json({ message: "User not found" })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const createUser = async (req, res) => {
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
+  })
+
+  try {
+    const newUser = await user.save()
+    res.status(201).json(newUser)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+export const updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.json(updatedUser)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.json({ message: "User deleted" })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
