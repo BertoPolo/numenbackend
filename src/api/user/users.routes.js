8bff4852b719050e5153c12e072306b62ae72d71
Alberto <bertopolo91@gmail.com>
2024-05-11 17:13:55 +0200

    fix number generated

--- src/api/user/users.routes.js
+++ src/api/user/users.routes.js
@@ -64,7 +64,7 @@ usersRouter.post("/", async (req, res, next) => {
       const newUser = new usersSchema({
         ...req.body,
         email: emailLowerCase,
-        randomNumber: Math.floor(Math.random() * 10000),
+        randomNumber: Math.floor(10000 + Math.random() * 90000),
       })
       const { _id } = await newUser.save()
 
