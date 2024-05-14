fa0a400c0742a788d1fb09bd9ecbfd4824aaf7d7
Alberto <bertopolo91@gmail.com>
2024-05-14 16:49:02 +0200

    update task controllers and simplify 2 endpoints

--- src/api/task/tasks.controller.js
+++ src/api/task/tasks.controller.js
@@ -1,55 +1,60 @@
 import Task from "./tasks.routes.js"
 
-export const getAllTasks = async (req, res) => {
+export const getSelfTasks = async (req, res, next) => {
   try {
-    const tasks = await Task.find()
+    const tasks = await Task.find({ createdBy: req.user._id })
     res.json(tasks)
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    next(new createError(500, error.message))
   }
 }
 
-export const getTaskById = async (req, res) => {
+export const getTaskById = async (req, res, next) => {
   try {
-    const task = await Task.findById(req.params.id)
-    if (task) {
-      res.json(task)
-    } else {
-      res.status(404).json({ message: "Task not found" })
+    const task = await Task.findOne({ _id: req.params.id, createdBy: req.user._id })
+    if (!task) {
+      return next(new createError(404, "Task not found"))
     }
+    res.json(task)
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    next(new createError(500, error.message))
   }
 }
 
-export const createTask = async (req, res) => {
-  const task = new Task({
-    title: req.body.title,
-    task: req.body.task,
+export const createTask = async (req, res, next) => {
+  const newTask = new Task({
+    ...req.body,
+    createdBy: req.user._id,
   })
 
   try {
-    const newTask = await task.save()
-    res.status(201).json(newTask)
+    const savedTask = await newTask.save()
+    res.status(201).json(savedTask)
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    next(new createError(400, error.message))
   }
 }
 
-export const updateTask = async (req, res) => {
+export const updateTask = async (req, res, next) => {
   try {
-    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, { ...req.body }, { new: true, runValidators: true })
+    if (!updatedTask) {
+      return next(new createError(404, "Task not found"))
+    }
     res.json(updatedTask)
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    next(new createError(400, error.message))
   }
 }
 
-export const deleteTask = async (req, res) => {
+export const deleteTask = async (req, res, next) => {
   try {
-    await Task.findByIdAndDelete(req.params.id)
-    res.json({ message: "Task deleted" })
+    const deletedTask = await Task.findByIdAndDelete(req.params.id)
+    if (!deletedTask) {
+      return next(new createError(404, "Task not found"))
+    }
+    res.status(200).json({ message: "Task deleted successfully" })
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    next(new createError(500, error.message))
   }
 }
--- src/api/task/tasks.routes.js
+++ src/api/task/tasks.routes.js
@@ -40,21 +40,32 @@ tasksRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
 
 //PUT Task
 tasksRouter.put("/:taskId", JWTAuthMiddleware, async (req, res, next) => {
+  // try {
+  //   const task = await tasksSchema.findOne({ _id: req.params.taskId, createdBy: req.user._id })
+  //   if (!task) {
+  //     return next(createError(404, "Task not found or you do not have permission to modify this task"))
+  //   }
+
+  //   const updatedTask = await tasksSchema.findByIdAndUpdate(
+  //     req.params.taskId,
+  //     {
+  //       ...req.body,
+  //     },
+  //     { new: true }
+  //   )
+  //   if (task) res.status(201).send(updatedTask)
+  //   else next(createError(404, `No task found`))
+  // } catch (error) {
+  //   console.log(error)
+  //   next(error)
+  // }
+
   try {
-    const task = await tasksSchema.findOne({ _id: req.params.taskId, createdBy: req.user._id })
-    if (!task) {
+    const updatedTask = await tasksSchema.findOneAndUpdate({ _id: req.params.taskId, createdBy: req.user._id }, req.body, { new: true })
+    if (!updatedTask) {
       return next(createError(404, "Task not found or you do not have permission to modify this task"))
     }
-
-    const updatedTask = await tasksSchema.findByIdAndUpdate(
-      req.params.taskId,
-      {
-        ...req.body,
-      },
-      { new: true }
-    )
-    if (task) res.status(201).send(updatedTask)
-    else next(createError(404, `No task found`))
+    res.status(201).send(updatedTask)
   } catch (error) {
     console.log(error)
     next(error)
@@ -63,17 +74,13 @@ tasksRouter.put("/:taskId", JWTAuthMiddleware, async (req, res, next) => {
 
 ///DELETE task
 tasksRouter.delete("/:taskId", JWTAuthMiddleware, async (req, res, next) => {
-  const task = await tasksSchema.findOne({ _id: req.params.taskId, createdBy: req.user._id })
-  if (!task) {
-    return next(createError(404, "Task not found or you do not have permission to modify this task"))
-  }
-
   try {
-    const tasksToDelete = await tasksSchema.findByIdAndDelete(req.params.taskId)
+    const deletedTask = await Task.findOneAndDelete({ _id: req.params.taskId, createdBy: req.user._id })
+    if (!deletedTask) {
+      return next(createError(404, "Task not found or you do not have permission to delete this task"))
+    }
 
-    if (tasksToDelete) {
-      res.status(200).send("Task was deleted successfully")
-    } else next(createError(404, `this task: ${req.params.taskId}, is not found`))
+    res.status(200).send("Task was deleted successfully")
   } catch (error) {
     next(error)
   }
