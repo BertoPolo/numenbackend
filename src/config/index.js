f5850ffd7b16a0dac708eb716025557c8c7a5d28
Alberto <bertopolo91@gmail.com>
2024-07-11 17:58:36 +0200

    fly

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,16 @@
-// import dotenv from "dotenv"
+import dotenv from "dotenv"
 
-// dotenv.config()
+dotenv.config()
 
 export default {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3004,
   dbURI: process.env.MONGO_CONNECTION,
   jwtSecret: process.env.JWT_SECRET,
-  frontendDevUrl: process.env.FE_DEV_URL,
+  frontendDevUrl: process.env.FE_DEV_URL || "http://localhost:3000",
   frontendProdUrl: process.env.FE_PROD_URL,
+}
+
+if (!dbURI || !jwtSecret || !frontendProdUrl) {
+  throw new Error("Missing required environment variables")
 }
