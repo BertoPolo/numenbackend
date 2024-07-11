ace75d4a17cb19d656d177d5e5d19bb59e52d45c
Alberto <bertopolo91@gmail.com>
2024-07-11 18:18:21 +0200

    api limiter improvement

--- src/middlewares/requestRestriction.js
+++ src/middlewares/requestRestriction.js
@@ -1,9 +1,16 @@
 import rateLimit from "express-rate-limit"
 
 const apiLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again after a break.",
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 100,
+  keyGenerator: function (req, res) {
+    return req.ip
+  },
+  handler: function (req, res, next) {
+    res.status(429).json({
+      message: "Too many requests, please try again later.",
+    })
+  },
 })
 
 export default apiLimiter
